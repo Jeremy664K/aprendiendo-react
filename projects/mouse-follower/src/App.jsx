6ac9950f90defb24b02d804294d2175e914206c5
Jeremy Díaz <jeremy664k@gmail.com>
2023-02-09 19:42:36 -0300

    Add mouse-follower project

--- projects/mouse-follower/src/App.jsx
+++ projects/mouse-follower/src/App.jsx
@@ -1,29 +1,42 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import { useEffect, useState } from "react";
 
-function App() {
-    const [count, setCount] = useState(0);
+const FollowMouse = () => {
+    const [enabled, setEnabled] = useState(false);
+    const [position, setPosition] = useState({ x: 0, y: 0 });
+    const textButtonEnabled = enabled ? "Desactivar puntero" : "Activar puntero";
+
+    const handleClick = () => {
+        setEnabled(!enabled);
+    };
+
+    useEffect(() => {
+        const handleMove = (e) => {
+            const { clientX, clientY } = e;
+            setPosition({ x: clientX, y: clientY });
+        };
+
+        if (enabled) {
+            window.addEventListener("pointermove", handleMove);
+        }
+
+        return () => {
+            window.removeEventListener("pointermove", handleMove);
+        };
+    }, [enabled]);
 
     return (
-        <div className="App">
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src="/vite.svg" className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://reactjs.org" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-                <p>
-                    Edit <code>src/App.jsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-        </div>
+        <>
+            <div className="mouse" style={{ transform: `translate(${position.x}px, ${position.y}px)` }} hidden={!enabled} />
+            <button onClick={handleClick}>{textButtonEnabled}</button>
+        </>
+    );
+};
+
+function App() {
+    return (
+        <main>
+            <FollowMouse />
+        </main>
     );
 }
 
