035cc179fc658c5123e3fe42b9b0642d2a31a8c8
Jeremy Díaz <jeremy664k@gmail.com>
2023-02-11 16:33:21 -0300

    Update test use data-testid attribute

--- projects/prueba-tecnica/src/App.jsx
+++ projects/prueba-tecnica/src/App.jsx
@@ -11,8 +11,12 @@ export function App() {
         <main className="Main">
             <section className="CatApp">
                 <h1 className="CatApp-title">CatApp</h1>
-                {fact && <p className="CatApp-fact">{fact}</p>}
-                {imageUrl && <img className="CatApp-img" src={imageUrl} alt={`Image extracted using the first word for ${fact}`} />}
+                {fact && (
+                    <p className="CatApp-fact" data-testid="fact">
+                        {fact}
+                    </p>
+                )}
+                {imageUrl && <img className="CatApp-img" src={imageUrl} alt={`Image extracted using the first word for ${fact}`} data-testid="image" />}
                 <button className="CatApp-btn" onClick={refreshFact}>
                     Get new fact
                 </button>
--- projects/prueba-tecnica/tests/example.spec.js
+++ projects/prueba-tecnica/tests/example.spec.js
@@ -1,17 +1,17 @@
 import { test, expect } from "@playwright/test";
 
 const CATAAS_URL = "https://cataas.com";
-const LOCALHOST_URL = "http://localhost:4000/";
+const LOCALHOST_URL = "http://localhost:5173/";
 
 test("app shows random fact and image", async ({ page }) => {
     await page.goto(LOCALHOST_URL);
 
-    const text = page.getByRole("paragraph");
-    const image = page.getByRole("img");
+    const fact = page.getByTestId("fact");
+    const image = page.getByTestId("image");
 
-    const textContent = await text.textContent();
+    const factContent = await fact.textContent();
     const imageSrc = await image.getAttribute("src");
 
-    expect(textContent?.length).toBeGreaterThan(0);
+    expect(factContent?.length).toBeGreaterThan(0);
     expect(imageSrc?.startsWith(CATAAS_URL)).toBeTruthy();
 });
