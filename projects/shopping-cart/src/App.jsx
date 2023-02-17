842bf08cdb96b78b5fae4c1737dadc977f1dde37
Jeremy Díaz <jeremy664k@gmail.com>
2023-02-17 19:38:18 -0300

    Feat render products and filters

--- projects/shopping-cart/src/App.jsx
+++ projects/shopping-cart/src/App.jsx
@@ -1,7 +1,29 @@
-import "./App.css";
+import Header from "./components/Header/Header";
+import Cart from "./components/Cart/Cart";
+import Products from "./components/Products/Products";
+import Footer from "./components/Footer/Footer";
+
+import { useFilters } from "./hooks/useFilters";
+import { IS_DEVELOPMENT } from "./config";
+
+import { products } from "./mocks/products.json";
+import { CartProvider } from "./context/cart";
 
 function App() {
-    return <h1>Shopping Cart</h1>;
+    const { filterProducts } = useFilters();
+
+    const filteredProducts = filterProducts(products);
+
+    return (
+        <CartProvider>
+            <Header />
+            <main className="Main">
+                <Cart />
+                <Products products={filteredProducts} />
+            </main>
+            {IS_DEVELOPMENT && <Footer />}
+        </CartProvider>
+    );
 }
 
 export default App;
--- projects/shopping-cart/src/main.jsx
+++ projects/shopping-cart/src/main.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
+import FiltersProviders from "./context/filters";
 
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
+    <FiltersProviders>
         <App />
-    </React.StrictMode>
+    </FiltersProviders>
 );
