58181475cc7c5661c88026aa028f95e12c35074e
Jeremy Díaz <jeremy664k@gmail.com>
2023-02-18 14:02:26 -0300

    Add cart context

--- projects/shopping-cart/src/context/cart.jsx
+++ projects/shopping-cart/src/context/cart.jsx
@@ -1,41 +1,18 @@
-import { createContext, useState } from "react";
+import { createContext } from "react";
+
+import { useCartReducer } from "../hooks/useCartReducer";
 
 export const CartContext = createContext();
 
 export function CartProvider({ children }) {
-    const [cart, setCart] = useState([]);
-
-    const addToCart = (product) => {
-        const productIndexInCart = cart.findIndex((item) => item.id === product.id);
-
-        if (productIndexInCart >= 0) {
-            const newCart = structuredClone(cart);
-            newCart[productIndexInCart].quantity += 1;
-
-            return setCart(newCart);
-        }
-
-        setCart((prevState) => [
-            ...prevState,
-            {
-                ...product,
-                quantity: 1
-            }
-        ]);
-    };
-
-    const removeToCart = (product) => setCart((prevState) => prevState.filter((item) => item.id !== product.id));
-
-    const checkProductInCart = (product) => cart.some((item) => item.id === product.id);
-
-    const clearCart = () => setCart([]);
+    const { cart, addToCart, removeFromCart, checkProductInCart, clearCart } = useCartReducer();
 
     return (
         <CartContext.Provider
             value={{
                 cart,
                 addToCart,
-                removeToCart,
+                removeFromCart,
                 checkProductInCart,
                 clearCart
             }}
