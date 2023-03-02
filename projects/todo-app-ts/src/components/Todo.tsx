07b0829ca4bb770c480d19e3ba2ba79b6a3217fb
Jeremy Díaz <jeremy664k@gmail.com>
2023-03-02 14:54:44 -0300

    Add id for checkbox and label

--- projects/todo-app-ts/src/components/Todo.tsx
+++ projects/todo-app-ts/src/components/Todo.tsx
@@ -1,3 +1,4 @@
+import { useId } from "react";
 import { type TodoId, type Todo as TodoTypes } from "../types";
 
 interface Props extends TodoTypes {
@@ -6,14 +7,16 @@ interface Props extends TodoTypes {
 }
 
 export const Todo: React.FC<Props> = ({ id, title, completed, onRemoveTodo, onToggleCompleteTodo }) => {
+    const checkboxCompletedId = useId();
+
     const handleChangeCheckbox = (e: React.ChangeEvent<HTMLInputElement>) => {
         onToggleCompleteTodo({ id, completed: e.target.checked });
     };
 
     return (
         <div className="view">
-            <input type="checkbox" className="toggle" checked={completed} onChange={handleChangeCheckbox} />
-            <label>{title}</label>
+            <input type="checkbox" className="toggle" id={checkboxCompletedId} checked={completed} onChange={handleChangeCheckbox} />
+            <label htmlFor={checkboxCompletedId}>{title}</label>
             <button
                 className="destroy"
                 onClick={() => {
